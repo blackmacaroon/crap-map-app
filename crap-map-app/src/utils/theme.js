c33e0b9cf58c0223057106652439a8bcfa8feabc
Kayla <6lackmacaroon@gmail.com>
2019-11-01 19:15:39 -0600

    install mui and create theme scaffold

---
+++ crap-map-app/src/utils/theme.js
@@ -0,0 +1,19 @@
+import { createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+    typography: {
+        fontFamily: ""
+    },
+    palette: {
+        primary: {
+            main: "",
+            contrastText: "",
+        },
+        secondary: {
+            main: "",
+            contrastText: "",
+        }
+    }
+})
+
+export default theme
