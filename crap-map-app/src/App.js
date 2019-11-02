3b3b53fad8089da1359ed58c123d197fcc8af104
Kayla <6lackmacaroon@gmail.com>
2019-11-01 19:23:38 -0600

    wrap app in router and mui theme

--- crap-map-app/src/App.js
+++ crap-map-app/src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { MuiThemeProvider } from "@material-ui/core"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          hi
-        </p>
-        
-      </header>
-    </div>
+    <Router>
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <header className="App-header">
+            <p>
+              hi
+            </p>
+            
+          </header>
+        </div>
+      </MuiThemeProvider>
+    </Router>
   );
 }
 
